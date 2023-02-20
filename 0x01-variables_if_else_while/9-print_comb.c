060d328dfde589affb631c25a7de84f3480ff432
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-02-20 03:38:11 +0200

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base 16 in lowercase, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+char c;
+
+while (i < 10)
+{
+c = i + '0';
+putchar(c);
+i++;
+}
+
+i = 97;
+while (i < 103)
+{
+c = i;
+putchar(c);
+i++;
+}
+
+putchar('\n');
+
+return (0);
+}
