5b8d5e29dd4a879cd181ce02ad35580d8a00840b
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-02-20 03:19:51 +0200

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,11 +19,11 @@ n = rand() - RAND_MAX / 2;
 
     /* Print whether the number is positive, negative or zero */
 if (n > 0)
-        printf("%d is positive\n", n);
+printf("%d is positive\n", n);
 else if (n == 0)
-        printf("%d is zero\n", n);
+printf("%d is zero\n", n);
 else
-        printf("%d is negative\n", n);
+printf("%d is negative\n", n);
 
 return (0);
 }
