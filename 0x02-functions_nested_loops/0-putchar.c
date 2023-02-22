fb0567260ab5a58253ff2a97404e84bb0eeba93c
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-02-22 02:29:59 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- * The main function of the code
+ * main - The main function of the code
  * Takes no parameters
- * always returns 0 (success)
+ *
+ * return - always returns 0 (success)
  */
-int main(void){
+int main(void)
+{
 	printf("_putchar\n");
 	return (0);
 }
