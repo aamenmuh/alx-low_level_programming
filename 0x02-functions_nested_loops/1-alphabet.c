0117a565d11a0f58631208c21a4247f8cbd9fb94
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-02-22 02:42:00 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- * print_alphabet - a function that prints all the alphabets
- * no parameters
- * 
- * Return: always returns 0 (success)
- * */
-void print_alphabet(void) {
+*print_alphabet - a function that prints all the alphabets
+*no parameters
+* 
+*Return: always returns 0 (success)
+* 
+*/
+void print_alphabet(void) 
+{
 	char letter = 'a';
 	while (letter <= 'z') {
 		printf("%c", letter);
