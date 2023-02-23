cd7f3960721c4535e0c5dd54709556a4ecf473f2
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-02-23 19:56:21 +0200

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * _isalpha - checks if the parameter is alphabetic
+ * @c: the parameter
+ * 
+ *
+ * Describtion: zeby
+ */
+
+int _isalpha(int c)
+{
+	r > 64 ? (r < 123 ? return (1)): return (0)
+}
