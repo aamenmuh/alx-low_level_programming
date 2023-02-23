492f8951493f79241dd14c15b1b7725c34971e96
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-02-23 20:52:56 +0200

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,11 +10,11 @@ for (hour = 0; hour < 24; hour++)
 {
 for (minute = 0; minute < 60; minute++)
 {
-_putchar((hour / 10) + '0');
-_putchar((hour % 10) + '0');
-_putchar(':');
-_putchar((minute / 10) + '0');
-_putchar((minute % 10) + '0');
+putchar((hour / 10) + '0');
+putchar((hour % 10) + '0');
+putchar(':');
+putchar((minute / 10) + '0');
+putchar((minute % 10) + '0');
 }
 }
 }
