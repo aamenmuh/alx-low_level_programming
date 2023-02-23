ed87f441bed65b77adf209d802515d3295319f4b
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-02-23 21:20:31 +0200

    Create 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+/**
+ * print_to_98 - prints numbers till 98
+ * @n : the para
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i < 98; i++){
+		printf('%d,', i);
+		printf(", ");
+	}
+	printf(i);
+}
