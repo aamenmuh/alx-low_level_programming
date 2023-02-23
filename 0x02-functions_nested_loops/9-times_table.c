cb35ed7adb5313d17740a7580e84f77bb074dc1e
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-02-23 21:09:33 +0200

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,16 +4,16 @@
  */
 void times_table(void)
 {
-	int i, j =0;
-	
-	for(j=0; j < 10; j++)
+	int i, j = 0;
+
+	for(j = 0; j < 10; j++)
 	{
-	for(i=0 ; i < 9; i++)
+	for(i = 0 ; i < 9; i++)
 	{
 		printf("%d", i * j);
 		(i + 1) * j < 10 ? printf(",  ") : printf(", ");
 	}
-	printf("%d", i*j);
+	printf("%d", i * j);
 	putchar('\n');
 }
 }
