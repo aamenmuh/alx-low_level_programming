a8dc46bd4fb31cd02d24aa4eaf7a1ac18d465ccd
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-02-23 20:27:16 +0200

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 /**
- * print_sign - checks the parity of the number
- * @n: the parameter
+ * print_sign - prints the sign of a number
  *
- * Return: returns 1, 0, or -1 (sucess).
+ * @n: the number to check
+ *
+ * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than zero
  */
+
 int print_sign(int n)
 {
+	int sign = 0;
+	
 	if (n > 0)
 	{
-		return (putchar('+'), 1);
+		sign = 1;
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		return (putchar('0'), 0);
+		sign = -1;
 	}
-	else
-	{
-		return (putchar('-'), -1);
-	}
+	
+	printf("%d%c", sign, (sign == 0 ? '0' : '+'));
+	return sign;
 }
