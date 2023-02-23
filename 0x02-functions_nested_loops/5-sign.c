80e4522e738457ee2e7bc4b5b8222a9444be7460
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-02-23 20:23:49 +0200

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
-		return (1);
+		return (putchar('+'),1);
 	}
 	else if (n == 0)
 	{
-		printf("0");
-		return (0);
+		return (putchar('0'),0);
 	}
 	else
 	{
-		printf("-");
-		return (-1);
+		return (putchar('-),-1);
 	}
 }
