e9222c415613e7589c248e1e48f1d5ad9ba60b07
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-02-23 21:29:22 +0200

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,12 +6,13 @@
 void print_to_98(int n)
 {
 	int i, step;
-	
-	n > 98 ? (step = -1) : (step = 1); 
-	for (i = n; i != 98; i += step){
+
+	n > 98 ? (step = -1) : (step = 1);
+	for (i = n; i != 98; i += step)
+	{
 		printf("%d", i);
 		printf(", ");
 	}
-	printf("%d",i);
+	printf("%d", i);
 	putchar('\n');
 }
