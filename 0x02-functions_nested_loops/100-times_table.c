8389e40e346908f6a86354796b3f3c782365e523
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-02-25 01:20:13 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,13 +7,15 @@ void print_times_table(int n)
 {
 	int i, j = 0;
 	
-	if (n > -1 && n < 16){
+	if (n > -1 && n < 16)
+	{
 		for (j = 0; j < (n + 1); j++)
 		{
 		for (i = 0; i < n; i++)
 		{
 			printf("%d", i * j);
-			(i + 1) * j < 10 ? printf(",   ") : ((i + 1) * j < 100 ? printf(",  ") : printf(", "));
+			(i + 1) * j < 10 ? printf(",   ") : ((i + 1) * j < 100 ? printf(",  ") \
+							     : printf(", "));
 		}
 		printf("%d", i * j);
 		putchar('\n');
