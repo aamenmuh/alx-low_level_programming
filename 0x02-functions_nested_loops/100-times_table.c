0df542b214af4c329d326aa6591a9438d32d369a
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-02-24 23:59:04 +0200

    Create 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,19 @@
+/**
+ * print_times_table  - prints the nth timetable of a number.
+ * @n: the para
+ */
+void print_times_table(int n)
+{
+	int i, j = 0;
+
+	for (j = 0; j < (n + 1); j++)
+	{
+	for (i = 0 ; i < n; i++)
+	{
+		printf("%d", i * j);
+		(i + 1) * j < (n + 1) ? printf(",  ") : printf(", ");
+	}
+	printf("%d", i * j);
+	putchar('\n');
+}
+}
