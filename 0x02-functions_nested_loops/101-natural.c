0fb2a3b1141fe76ed17f2437bb6cbac7a2486ed2
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-02-25 00:05:07 +0200

    Create 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int sum, i;
+	
+	for (i = 3; i < 1024; i++)
+	{
+		if ( i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d",i);
+
+	return (0);
+}
