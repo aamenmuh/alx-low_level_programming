a1ea763fe67ee12df50c741a8eb5f4cb6bd2e964
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-02-25 03:49:10 +0200

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * _isUpper - checks if upper
- * @c: para
- *
- * Return: 1 or 0
- */
+* _isUpper - checks if upper
+* @c: para
+*
+* Return: 1 or 0
+*/
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
