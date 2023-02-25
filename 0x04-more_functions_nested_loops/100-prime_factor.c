f26bf5df7746df4b9d42d950c599ccaaf10f5462
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-02-25 04:24:49 +0200

    Create 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Finds the largest prime factor of 612852475143
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	const long N = 612852475143;
+	long i, largest_factor;
+
+	largest_factor = 1;
+
+	for (i = 2; i <= N; i++)
+	{
+		while (N % i == 0)
+		{
+			largest_factor = i;
+			N /= i;
+		}
+	}
+
+	printf("%ld\n", largest_factor);
+
+	return (0);
+}
