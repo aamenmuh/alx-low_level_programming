c2ea62bdcbdf1a7ee599f39405a8386d66265b0d
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-02-25 04:11:57 +0200

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -25,30 +25,4 @@ void print_diagonal(int n)
 		putchar('\\');
 		putchar('\n');
 	}
-}/**
- * print_diagonal - draws a diagonal line of n backslashes in the terminal
- *
- * @n: the number of backslashes to print
- *
- * Return: void
- */
-void print_diagonal(int n)
-{
-	int i, j;
-
-	if (n <= 0)
-	{
-		putchar('\n');
-		return;
-	}
-
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < i; j++)
-		{
-			putchar(' ');
-		}
-		putchar('\\');
-		putchar('\n');
-	}
 }
