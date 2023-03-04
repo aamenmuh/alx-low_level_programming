05c67ecb0e673a408d17f07d68900cda52378ce4
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-03-04 14:26:28 +0200

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,9 +4,7 @@
  * @s1: Pointer to the first string.
  * @s2: Pointer to the second string.
  *
- * Return: Negative value if s1 is less than s2,
- *         0 if s1 is equal to s2,
- *         Positive value if s1 is greater than s2.
+ * Return: N, less, P, more, 0 equ
  */
 int _strcmp(char *s1, char *s2)
 {
