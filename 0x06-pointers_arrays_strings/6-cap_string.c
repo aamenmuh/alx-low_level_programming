dc3c5b39c15a974e5bb3c8fa3d009cc21534ab39
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-03-04 16:10:25 +0200

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,7 +3,7 @@
  * cap_string - Capitalizes all words of a string.
  * @str: Pointer to the string.
  *
- * Returnss: Pointer to the resulting string.
+ * Return: Pointer to the resulting string.
  */
 char *cap_string(char *str)
 {
@@ -11,14 +11,14 @@ char *cap_string(char *str)
 
 		for (i = 0; str[i] != '\0'; i++)
 		{
-				if ((str[i - 1] == ' ' || str[i - 1] == '\t' ||
-					str[i - 1] == '\n' || str[i - 1] == ',' ||
-					str[i - 1] == ';' || str[i - 1] == '.' ||
-					str[i - 1] == '!' || str[i - 1] == '?' ||
-					str[i - 1] == '"' || str[i - 1] == '(' ||
-					str[i - 1] == ')' || str[i - 1] == '{' ||
-					str[i - 1] == '}') && str[i] >= 'a' && str[i] <= 'z')
-						str[i] = str[i] - 32;
+			if ((str[i - 1] == ' ' || str[i - 1] == '\t' ||
+				str[i - 1] == '\n' || str[i - 1] == ',' ||
+				str[i - 1] == ';' || str[i - 1] == '.' ||
+				str[i - 1] == '!' || str[i - 1] == '?' ||
+				str[i - 1] == '"' || str[i - 1] == '(' ||
+				str[i - 1] == ')' || str[i - 1] == '{' ||
+				str[i - 1] == '}') && str[i] >= 'a' && str[i] <= 'z')
+					str[i] = str[i] - 32;
 		}
 
 		return (str);
