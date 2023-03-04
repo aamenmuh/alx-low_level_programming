bccf750604ebecc76675bf84cac0090a7fe3152e
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-03-04 15:08:10 +0200

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,13 +11,13 @@ char *cap_string(char *str)
 
 		for (i = 0; str[i] != '\0'; i++)
 		{
-				if (str[i - 1] == ' ' || str[i - 1] == '\t' ||
+				if ((str[i - 1] == ' ' || str[i - 1] == '\t' ||
 					str[i - 1] == '\n' || str[i - 1] == ',' ||
 					str[i - 1] == ';' || str[i - 1] == '.' ||
 					str[i - 1] == '!' || str[i - 1] == '?' ||
 					str[i - 1] == '"' || str[i - 1] == '(' ||
 					str[i - 1] == ')' || str[i - 1] == '{' ||
-					str[i - 1] == '}' && str[i] >= 'a' && str[i] <= 'z')
+					str[i - 1] == '}') && str[i] >= 'a' && str[i] <= 'z')
 						str[i] = str[i] - 32;
 		}
 
