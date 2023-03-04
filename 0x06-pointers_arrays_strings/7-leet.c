213714c42675b2bda3d2bccc2df5b441845b1272
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-03-04 17:03:37 +0200

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
+
 /**
- * leet - encodes a string
- * @str: pointer to the string
+ * leet - encodes a string into 1337
+ * @str: pointer to the string to encode
  *
- * Return: returns the encoded string
+ * Return: pointer to the encoded string
  */
 char *leet(char *str)
 {
 	int i, j;
 	char lowerCase[] = {'a', 'e', 'o', 't', 'l', '\0'};
-	char upperCase[] = {'A', 'E', 'O', 'T', 'L', '\0'}; 
+	char upperCase[] = {'A', 'E', 'O', 'T', 'L', '\0'};
 	char encoding[] = {'4', '3', '0', '7', '1', '\0'};
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; j < 5 ; j++)
+		for (j = 0; j < 5; j++)
 		{
 			if (str[i] == lowerCase[j] || str[i] == upperCase[j])
+			{
 				str[i] = encoding[j];
+			}
 		}
 	}
-
 	return (str);
 }
