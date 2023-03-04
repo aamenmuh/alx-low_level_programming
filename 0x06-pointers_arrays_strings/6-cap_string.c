fa63fbf8f5632b6511b13956b5f8d0401631c1c4
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-03-04 16:11:36 +0200

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,19 +7,19 @@
  */
 char *cap_string(char *str)
 {
-		int i;
+	int i;
 
-		for (i = 1; str[i] != '\0'; i++)
-		{
-			if ((str[i - 1] == ' ' || str[i - 1] == '\t' ||
-				str[i - 1] == '\n' || str[i - 1] == ',' ||
-				str[i - 1] == ';' || str[i - 1] == '.' ||
-				str[i - 1] == '!' || str[i - 1] == '?' ||
-				str[i - 1] == '"' || str[i - 1] == '(' ||
-				str[i - 1] == ')' || str[i - 1] == '{' ||
-				str[i - 1] == '}') && str[i] >= 'a' && str[i] <= 'z')
-					str[i] = str[i] - 32;
-		}
+	for (i = 1; str[i] != '\0'; i++)
+	{
+		if ((str[i - 1] == ' ' || str[i - 1] == '\t' ||
+			str[i - 1] == '\n' || str[i - 1] == ',' ||
+			str[i - 1] == ';' || str[i - 1] == '.' ||
+			str[i - 1] == '!' || str[i - 1] == '?' ||
+			str[i - 1] == '"' || str[i - 1] == '(' ||
+			str[i - 1] == ')' || str[i - 1] == '{' ||
+			str[i - 1] == '}') && str[i] >= 'a' && str[i] <= 'z')
+				str[i] = str[i] - 32;
+	}
 
-		return (str);
+	return (str);
 }
