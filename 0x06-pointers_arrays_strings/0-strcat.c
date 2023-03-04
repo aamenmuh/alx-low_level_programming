1ff20987e2e3f8f7a75b31754798ce3c82d9e6cb
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-03-04 14:15:31 +0200

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,10 +14,12 @@ char *_strcat(char *dest, char *src)
 	{
 		destLen++;
 	}
+
 	for(i = 0; src[i] != '\0'; i++)
 	{
 		dest[destLen + i] = src[i];
 	}
+
 	dest[destLen + i] = '\0';
 
 	return (dest);
