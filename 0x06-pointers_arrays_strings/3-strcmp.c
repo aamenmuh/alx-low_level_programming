a7a1f30c86c7e342310ddcf55e73b651c6127ce2
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-03-04 14:23:37 +0200

    Create 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * _strcmp - Compares two strings.
+ * @s1: Pointer to the first string.
+ * @s2: Pointer to the second string.
+ *
+ * Return: Negative value if s1 is less than s2,
+ *         0 if s1 is equal to s2,
+ *         Positive value if s1 is greater than s2.
+ */
+int _strcmp(char *s1, char *s2)
+{
+		int i;
+
+		for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+		{
+			if (s1[i] != s2[i])
+				{
+					break;
+				}
+		}
+
+		return s1[i] - s2[i];
+}
