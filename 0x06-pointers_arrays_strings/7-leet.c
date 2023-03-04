cb88eca6c6c5eea9c3f57f8c0901e8c979a9b72e
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-03-04 16:59:51 +0200

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,10 +8,11 @@
 char *leet(char *str)
 {
 	int i, j;
-	
+
 	char lowerCase[] = {'a', 'e', 'o', 't', 'l', '\0'};
 	char upperCase[] = {'A', 'E', 'O', 'T', 'L', '\0'}; 
 	char encoding[] = {'4', '3', '0', '7', '1', '\0'};
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; j < 5 ; j++)
