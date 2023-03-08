c64c3bf8e705d3b5a6dc363e3e6a83f5d5730386
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-03-08 12:33:10 +0200

    Create 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+int _sqrt_recursion(int n)
+{
+	if (n == 0 || n == 1)
+		return (n);
+	else if (n < 0)
+		return (-1);
+	else 
+	{
+		int halfRoot = _sqrt_recursion(n / 2);
+		if (halfRoot * halfRoot == n)
+			return (halfRoot);
+		else if (halfRoot % 2 == 0)
+			return (2 * halfRoot);
+		else if (halfRoot % 2 == 1)
+			return (2 * halfRoot + 1);
+		else
+			return (-1);
+	}
+
+}
