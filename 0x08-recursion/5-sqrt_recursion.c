a7a119a2fba0db5d1360730093e913e18357b83e
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-03-08 12:48:17 +0200

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,9 +9,9 @@ int _sqrt_recursion(int n)
 		int halfRoot = _sqrt_recursion(n / 2);
 		if (halfRoot * halfRoot == n)
 			return (halfRoot);
-		else if (halfRoot % 2 == 0 && halfRoot * halfRoot > n)
+		else if (halfRoot % 2 == 0)
 			return (2 * _sqrt_recursion(halfRoot));
-		else if (halfRoot % 2 == 1 && halfRoot * halfRoot > n)
+		else if (halfRoot % 2 == 1)
 			return (2 * _sqrt_recursion(halfRoot) + 1);
 		else
 			return (-1);
