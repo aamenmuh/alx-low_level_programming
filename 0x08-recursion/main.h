0fe1d2bf09636c83d93e606e7b7cca5d6bf7c7ff
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-03-09 04:00:13 +0200

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,3 +5,5 @@ int _pow_recursion(int x, int y);
 int factorial(int n);
 int _strlen_recursion(char *s);
 void _print_rev_recursion(char *s);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
