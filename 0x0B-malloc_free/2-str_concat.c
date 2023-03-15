d8de0da7489d4c81cf63bf03fdb742e41387e501
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-03-15 02:33:50 +0200

    Create 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char *str_concat(char *s1, char *s2)
+{
+	char* array;
+	int i;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	array = (char *)malloc(strlen(s1) + 1);
+	array = realloc(array, strlen(s2) + 1);
+
+	for (i = 0; i < strlen(s1) + strlen(s2) + 1; i++)
+	{
+		if(i < strlen(s1) + 1)
+			array[i] = s1[i];
+		else
+			array[i] = s2[i - strlen(s1) - 1];
+	}
+
+	return (array)
+}
