5ea61c4d8ee0ad9ad2b3d9318978ae5817abb397
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-03-17 03:08:14 +0200

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,11 +26,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (pointer == NULL)
 		return (NULL);
 
-	for (i = 0; i < strlen(s1) + n + 1)
+	for (i = 0; i < strlen(s1) + n + 1; i++)
+	{
 		if (i < strlen(s1))
 			pointer[i] = s1[i];
 		else
 			pointer[i] = s2[i];
-	
+	}
+
 	return (pointer);
 }
