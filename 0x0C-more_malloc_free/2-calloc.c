03b544ed6d2749ba2f49f0c97897dee42007b5b4
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-03-18 15:48:37 +0200

    Create 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc: allocated memory for array
+ * @nmemb: number of array elements
+ * @size: memory size
+ *
+ * Return: depends on the input
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *pointer;
+
+	if(nmemb == 0 || size == 0)
+		return (NULL);
+
+	pointer = malloc(nmemb * size);
+
+	if(pointer == NULL)
+		return (NULL);
+
+	return (pointer);
+}
