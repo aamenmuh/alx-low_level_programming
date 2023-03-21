8bde4b510a776f5a834df2c4c1c885dc4089014e
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-03-21 02:41:57 +0200

    Update 4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,3 +1,10 @@
+/**
+ * SUM - adds two numbers
+ * @x: first num
+ * @y: second bum
+ *
+ * Return: x + y
+ */
 #ifndef SUM
-#define SUM(x, y) ((x) + (y)) /*adds two numbers */
+#define SUM(x, y) ((x) + (y))
 #endif
