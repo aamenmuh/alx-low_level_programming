1373580997f81c768ba86c4ea818fe1a5fd74426
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-03-26 14:32:45 +0200

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,27 +9,25 @@
  */
 int main(int argc, char **argv)
 {
-	/* Check if number of arguments is correct */
+	int i;
+
 	if (argc != 2)
 	{
 		fprintf(stderr, "Error\n");
 		eixt (1);
 	}
 
-	/* Convert argument to integer */
 	int num_bytes = atoi(argv[1]);
 
-	/* Check if argument is negative */
 	if (num_bytes < 0)
 	{
 		fprintf(stderr, "Error\n");
 		exit (2);
 	}
 
-	/* Print opcodes */
 	unsigned char *ptr = (unsigned char *)main;
 
-	for (int i = 0; i < num_bytes; i++)
+	for (i = 0; i < num_bytes; i++)
 	{
 		printf("%02x ", *ptr);
 		ptr++;
