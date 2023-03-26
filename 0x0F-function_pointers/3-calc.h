96930764fe71644d2a829f64b2ef1d03168aa9db
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-03-26 13:37:45 +0200

    Update 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,3 +1,5 @@
+#ifndef calc
+#define calc
 /**
  * struct op - Struct op
  *
@@ -15,3 +17,6 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+
+#endif
