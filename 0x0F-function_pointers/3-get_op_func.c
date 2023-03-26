32c09086cce9315472988fbec23adc15f3d857d0
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-03-26 14:04:15 +0200

    Create 3-get_op_func.c

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,26 @@
+/**
+ * get_op_func - selects the correct operation
+ * @s: string input
+ *
+ * Return: returns zeby
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+    };
+    int i;
+
+    while (ops[i].op != NULL)
+    {
+    	if (*s == *(ops[i].op) && *(s + 1) == '\0')
+    		return (ops[i].f);
+    	i = i + 1;
+    }
+    return (NULL);
+}
