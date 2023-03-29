9726b42210b78dc965db84ec8fa2025ef5e3319b
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-03-29 03:14:06 +0200

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,13 +1,11 @@
-#include <string.h>
 int is_palindrome(char *s)
 {
-	int length = strlen(s);
-
-	if (length == 0)
-		return (1);
-	if (s[0] != s[length - 1])
-		return (0);
-
-	s[length - 1] = '\0';
-	return is_palindrome(s + 1);
+    if (*s == '\0' || *(s + 1) == '\0') {
+        return 1;  // an empty string or a single character string is a palindrome
+    } else if (*s != *(s + strlen(s) - 1)) {
+        return 0;  // the string is not a palindrome if the first and last characters differ
+    } else {
+        *(s + strlen(s) - 1) = '\0';  // temporarily remove the last character from the string
+        return is_palindrome(s + 1);  // recursively check if the substring without the first and last characters is a palindrome
+    }
 }
