6d43934160b4ff0550841ea1768829932e24ea0b
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-04-12 04:11:48 +0200

    Create 5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * clear_bit - sets the bit to 0
+ * @n: number in denary
+ * @index: position to set
+ *
+ * Return: returns the new number
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 33)
+		return (-1);
+	*n ^= (1UL << index);
+	return (1);
+}
