c9f70b71cd63a4bbde2f858b4bb4abfecb27cec7
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-04-12 03:49:06 +0200

    Create 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,10 @@
+/**
+ * print_binary - prints binary rep of number
+ * @n: number
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) ? '1' : '0');
+}
