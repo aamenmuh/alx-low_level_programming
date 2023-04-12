d90431ff50104cf45b8bd6d68aea076b00866b6f
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-04-12 04:12:15 +0200

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * clear_bit - sets the bit to 0
+ * flip_bit - sets the bit to 0
  * @n: number in denary
  * @index: position to set
  *
  * Return: returns the new number
  */
-int clear_bit(unsigned long int *n, unsigned int index)
+int flip_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 33)
 		return (-1);
