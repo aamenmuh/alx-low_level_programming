34faf0b77dc28c58464285e575567087c351fd14
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-04-12 04:05:25 +0200

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * set_bit: sets the bit to 1
+ * set_bit - sets the bit to 1
  * @n: number in denary
  * @index: position to set
  *
@@ -9,8 +9,7 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 33)
-		return (-1);
-	
+		return (-1);	
 	*n |= (1UL << index);
 	return (1);
 }
