233307304441ee1a43fe3ee8aa377164b7d50c9e
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-04-12 04:00:29 +0200

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,11 +3,12 @@
  * @n: value in denary
  * @index: positionf of bit to be found
  *
- * Return: returns the value of the bit at the given position.
+ * Return: returns the bit at index
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index > 33)
 		return (-1);
+
 	return ((n >> index) & 1);
 }
