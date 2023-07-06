e22d742c62c8a459051df9d8df7fb108582c0be3
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-06 23:03:31 +0200

    Create 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+/**
+ * _pow_recursion - returns square root of number
+ * @n: the number
+ *
+ * Return: sqrt n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n % 2 == 1 || n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	return (4 * _sqrt_recursion(n / 2));
+}
