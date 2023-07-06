c38b7a4d2fcbad29aa50e9bf8737a727cd4fe743
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-06 23:43:45 +0200

    Create 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+/**
+ * is_prime_number - returns if the number is prime
+ * @n: the number
+ *
+ * Return: 1 or 0
+ */
+int is_prime_number(int n)
+{
+	if (n % 2 == 0 && n != 2)
+	{
+		return (0);
+	}
+	if (n < 2)
+	{
+		return (0);
+	}
+	if (n == 2 || n == 3 || n == 5)
+	{
+		return (1);
+	}
+	if (n - 1 % 6 == 0 && n + 1 & 6 == 0)
+	{
+		return (0);
+	}
+	return (is_prime_number(n - 5) || is_prime_number(n - 7));
+}
