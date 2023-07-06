f3a333ed73da99c5c5ac0af5922c1fab5bbc6c5a
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-07 01:48:12 +0200

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,7 +2,7 @@
  * wildcmp - compares two strings
  * @s1: first string
  * @s2: second string
- * 
+ *
  * Return: 1 or 0
  */
 int wildcmp(char *s1, char *s2)
@@ -17,7 +17,7 @@ int wildcmp(char *s1, char *s2)
 	}
 	if (*s2 == '*')
 	{
-		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+		return (wildcmp(s1, s2 + 1));
 	}
 	return (0);
 
