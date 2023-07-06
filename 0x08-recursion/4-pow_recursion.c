57120a4e7af600a1638f95086dcc3404ec00aeaf
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-06 22:57:46 +0200

    Create 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+/**
+ * _pow_recursion - returns x ^ y
+ * @x: the base
+ * @y: the power
+ *
+ * Return: x raised to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(y - 1));
+}
