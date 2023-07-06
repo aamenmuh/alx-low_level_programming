3384caf7102fa1306ea35a297dc52a5966b45032
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-06 23:48:31 +0200

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,7 +6,7 @@
  */
 int is_prime_number(int n)
 {
-	if (n % 2 == 0 && n != 2)
+	if ((n % 2) == 0 && n != 2)
 	{
 		return (0);
 	}
@@ -18,7 +18,7 @@ int is_prime_number(int n)
 	{
 		return (1);
 	}
-	if (n - 1 % 6 == 0 && n + 1 & 6 == 0)
+	if ((n - 1) % 6 == 0 && (n + 1) % 6 == 0)
 	{
 		return (0);
 	}
