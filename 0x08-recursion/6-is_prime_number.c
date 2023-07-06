7be96d5099d2c1170a2d65a6615bbe999b8dbccf
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-07 01:16:42 +0200

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,25 @@
 /**
+ * helperPrime - a helper function with two paras
+ * @n: the number
+ * @divisor: the divisor.
+ *
+ * Return: 1 or 0
+ */
+int helperPrime(int n, int divisor)
+{
+	if (divisor == 1)
+	{
+		return (1);
+	}
+
+	if (n % divisor == 0)
+	{
+		return (0);
+	}
+
+	return (helperPrime(n, divisor - 1));
+}
+/**
  * is_prime_number - returns if the number is prime
  * @n: the number
  *
@@ -6,17 +27,5 @@
  */
 int is_prime_number(int n)
 {
-	if ((n % 2) == 0 && n != 2)
-	{
-		return (0);
-	}
-	if (n < 2)
-	{
-		return (0);
-	}
-	if (n == 2)
-	{
-		return (1);
-	}
-	return is_prime_number(n - 1);
+	return (helperPrime(n,n - 1));
 }
