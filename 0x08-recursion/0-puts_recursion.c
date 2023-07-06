9e0eef1b589cc54fcb88dabd281c987f1b7dd76f
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-06 22:32:25 +0200

    Create 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+ /**
+  * _puts_recursion - recursively prints a string 
+  * @s: pointer to the string
+  */
+void _puts_recursion(char *s){
+	if (*s == "\0"){
+		prtintf('\n');
+		break;
+	}
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
