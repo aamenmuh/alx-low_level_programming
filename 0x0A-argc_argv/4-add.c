cc9ab69d6c9f217d7139012078e632bb31785b86
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-12 21:19:30 +0200

    Create 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - the main function
+ * @argc: the number of args
+ * @argv: the arguments
+ *
+ * Return: 0 always success
+ */
+int main(int argc, char *argv[])
+{
+	int result, i;
+	char *arg;
+
+	*arg = arg[i];
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	for (i = 1; i < argc; i++)
+	{
+		while (*arg)
+		{
+			if (!isdigit((*arg)))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			arg++;
+		}
+		result += atoi(argv[i]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
