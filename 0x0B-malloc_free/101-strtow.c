5b02c687ed1b80b5b3b3e12940e9265f8d488c7c
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-13 01:18:34 +0200

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,46 +9,33 @@
 char **strtow(char *str)
 {
 	char **words;
-	int wordNum, i, cI, size, k;
+	int wordNum, i, cI, size, k, z;
 
 	cI = 0;
+	z= 0;
 	if (str == NULL || strlen(str) == 0)
-	{
 		return (NULL);
-	}
 	size = strlen(str);
 	for (i = 0; i < size; i++)
-	{
 		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-		{
 			wordNum++;
-		}
-	}
 	if (wordNum == 0)
-	{
 		return (NULL);
-	}
 	words = (char **)malloc((wordNum + 1) * sizeof(char *));
 	if (words == NULL)
-	{
 		return (words);
-	}
 	for (i = 0; i < size; i++)
 	{
 		if (str[i] != ' ')
 		{
 			k = i;
 			while (str[i] != ' ' && str[i] != '\0')
-			{
 				i++;
-			}
 			words[cI] = (char *)malloc((i - k + 1) * sizeof(char));
 			if (words[cI] == NULL)
 			{
 				for (i = 0; i < cI; i++)
-				{
 					free(words[cI]);
-				}
 				free(words);
 				return (NULL);
 			}
