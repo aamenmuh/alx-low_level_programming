f0f76fcac1776d5b3966885e1c9558bde061c9f0
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-13 00:18:59 +0200

    Create 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+/**
+ * alloc_grid: mem for grid
+ * @width: width
+ * @height: height
+ *
+ * Return: the grid
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width < 1 || height < 1)
+	{
+		return (NULL);
+	}
+	grid = (int **)malloc(height * sizeof(int *));
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)calloc(width, sizeof(int));
+
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL)
+		}
+	}
+	return (grid);
+}
