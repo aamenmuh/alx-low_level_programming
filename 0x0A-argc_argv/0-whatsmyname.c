4c3332691f7451b1bfd4633d99be2ad9825456e7
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-12 20:47:20 +0200

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,13 +4,13 @@
  * @argc: the number of args
  * @argv: the arguments
  *
- * Returns: 0 always success
+ * Return: 0 always success
  */
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
 	{
-		printf("%s",argv[0]);
+		printf("%s", argv[0]);
 	}
 	putchar('\n');
 	return (0);
