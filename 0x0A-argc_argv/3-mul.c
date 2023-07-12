0298970096d043e13b10385d1e5220d542586dac
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-12 21:05:46 +0200

    Create 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * @argc: the number of args
+ * @argv: the arguments
+ *
+ * Return: 0 always success
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	result = argv[1] * argv[2];
+	printf("%d\n," result);
+	return (0);
+}
