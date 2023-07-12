8bdeca6b85711ac1b6e0745a7231ca43c9dd8192
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-12 20:38:16 +0200

    Create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+/**
+ * main - the main function
+ * @argc: the number of args
+ * @argv: the arguments
+ *
+ * Return: 0 always success
+ */
+int main(int argc, char *argv[])
+{
+	printf(argv[0]);
+	printf('\n');
+	return (0);
+}
