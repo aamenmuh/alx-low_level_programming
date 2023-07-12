17adb4fa6aefe03f9bad4ee06151df8ad8c17a3b
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-12 20:49:00 +0200

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * @argc: the number of args
+ * @argv: the arguments
+ *
+ * Return: 0 always success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
