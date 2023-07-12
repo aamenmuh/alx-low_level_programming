d70bc210e4cf73774bb63c1348c8cf461a236123
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-12 23:47:49 +0200

    Create 0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+/**
+ * create_array - creates an array
+ * @size: size of array
+ * @c: inital chars
+ *
+ * 
+ * Return: returns pointer to array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+	int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	ar = malloc(size);
+
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i ++)
+	{
+		ar[i] = c;
+	}
+
+	return (*ar);
+}
