529ee4c982ab70b5b37ce404cdfe0efe8827d588
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-15 02:41:20 +0200

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,7 +10,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *arr;
 	size_t i, fullSize;
-	char* ptr;
+	char *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	arr = malloc(fullSize);
 	if (arr == NULL)
 		return (arr);
-	ptr = (char *) arr;
+	ptr = (char *)arr;
 	for (i = 0; i < fullSize; i++)
 		ptr[i] = 0;
 	return (arr);
