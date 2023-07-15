a6b712d28ad0641ae6ab18877d7e85ddf880d94c
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-15 02:19:42 +0200

    Create 1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,30 @@
+#include <string.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - concat strings
+ * @s1: first string
+ * @s2: second string
+ * @n: bytes
+ *
+ * Return: returns concat string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int size, i;
+	char *s;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	size = 0;
+	if (n > strlen(s2) - 1)
+		size += strlen(s2);
+	else
+		size += n;
+	size += strlen(s1);
+	s = malloc(size + 1);
+	if (s == NULL)
+		return (s);
+	return (s);
+}
