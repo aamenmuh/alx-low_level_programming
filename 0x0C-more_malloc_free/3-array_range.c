dee6ae30ae690624eee85311f06daab189b6564d
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-15 02:45:24 +0200

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	size = max - min + 1;
-	arr = malloc(size);
+	arr = malloc(size * sizeof(int));
 	if (arr == NULL)
 		return (arr);
 	for (i = 0; i < size; i++)
