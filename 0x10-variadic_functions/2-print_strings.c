dc2841dfeb5ce589d9443b842a3c881fb43bc989
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-21 03:37:08 +0200

    Create 2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - prints strings
+ * @separator: separator char
+ * @n: number of strings
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(ap, char *);
+		if (separator != NULL && i < n - 1)
+			if (s == NULL)
+				printf("(nil)%s", separator);
+			else
+				printf("%s%s", s, separator);
+	}
+	va_end(ap);
+	printf("\n");
+}
