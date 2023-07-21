379a3d7803236fc35de59d1fa7cc902f729c5d88
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-21 03:18:05 +0200

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef VARFUN_H
+#define VARFUN_H
 int sum_them_all(const unsigned int n, ...);
 void print_all(const char * const format, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
