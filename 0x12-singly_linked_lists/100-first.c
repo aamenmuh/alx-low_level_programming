26d494a1b951cbf01e3021d90198187f97136e1e
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-28 04:51:15 +0300

    Create 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+void preMain() __attribute((constructor));
+/**
+ * preMain - executes before main
+ */
+void preMain()
+{
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+}
