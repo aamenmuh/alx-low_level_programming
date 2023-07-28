82fcde2a19e3664ab064c68b294c60d87b684a11
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-28 05:02:52 +0300

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,11 @@
 .LC0:
-        .string "Hello, Holberton"
+		.string "Hello, Holberton\n"
 main:
-        push    rbp
-        mov     rbp, rsp
-        mov     edi, OFFSET FLAT:.LC0
-        mov     eax, 0
-        call    printf
-        mov     eax, 0
-        pop     rbp
-        ret
+		push	rbp
+		mov		rbp, rsp
+		mov		edi, OFFSET FLAT:.LC0
+		mov		eax, 0
+		call	printf
+		mov		eax, 0
+		pop		rbp
+		ret
