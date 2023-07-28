09e0b6aca24e078995212502c888c2c8aec0fbc9
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-28 04:52:44 +0300

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-void preMain() __attribute__((constructor));
+void preMain(void) __attribute__((constructor));
 /**
  * preMain - executes before main
  */
-void preMain()
+void preMain(void)
 {
 	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
 }
