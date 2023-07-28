b01663ccbab374f167abd12524cebcc4ee8bb13e
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-28 04:08:11 +0300

    Create 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * list_len - returns length of list
+ * @paramh: pointer to list
+ *
+ * Return: returns the length
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
