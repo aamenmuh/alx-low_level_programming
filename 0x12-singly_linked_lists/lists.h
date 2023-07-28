493a955f16416e46415ff76e9cdd4977c63dfd34
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-28 04:03:21 +0300

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-size_t print_list(const list_t *h);
-void free_list(list_t *head);
-list_t *add_node_end(list_t **head, const char *str);
-list_t *add_node(list_t **head, const char *str);
-size_t list_len(const list_t *h);
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -19,4 +14,9 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+size_t print_list(const list_t *h);
+void free_list(list_t *head);
+list_t *add_node_end(list_t **head, const char *str);
+list_t *add_node(list_t **head, const char *str);
+size_t list_len(const list_t *h);
 #endif
