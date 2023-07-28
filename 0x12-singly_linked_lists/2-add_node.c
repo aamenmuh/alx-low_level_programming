304e64cc35de444de282223c291e1005369b32ce
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-28 04:24:03 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,9 +11,11 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *newNode;
+
 	if (str == NULL)
 		return (NULL);
-	list_t *newNode = malloc(sizeof(list_t));
+	newNode = malloc(sizeof(list_t));
 	if (newNode == NULL)
 		return (NULL);
 	newNode->str = strdup(str);
