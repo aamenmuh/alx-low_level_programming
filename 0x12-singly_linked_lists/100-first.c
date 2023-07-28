854933cb6712e382e2457ce4299b3f08826f41a9
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-07-28 04:54:15 +0300

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,5 +7,5 @@ void preMain(void) __attribute__((constructor));
  */
 void preMain(void)
 {
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
