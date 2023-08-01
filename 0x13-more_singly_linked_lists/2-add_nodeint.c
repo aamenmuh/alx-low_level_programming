7acd58e76a1bf653a25ed6ddc13da8b3a682c2eb
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-08-01 23:58:47 +0200

    Create 2-add_nodeint.c

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_nodeint - adds node to begin
+ * @head: head of list
+ * @n: value of node
+ *
+ * Return: the new node
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newNode;
+
+	newNode->n = n;
+
+	if (*head == NULL)
+	{
+		newNode->next = NULL;
+		return (*newNode);
+	}
+	newNode->next = *head;
+	return (*newNode);
+
+}
