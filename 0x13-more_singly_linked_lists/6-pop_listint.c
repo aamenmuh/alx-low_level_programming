76194ac03399b11d33177750036eed6c763ec41e
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-08-02 01:15:18 +0200

    Create 6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * pop_listint - remove head
+ * @head: head
+ *
+ * Return: returns removed element
+ */
+int pop_listint(listint_t **head){
+	int data;
+	listint_t *current;
+
+	current = *head;
+	if (head == NULL)
+		return (0);
+	data = current->n;
+	*head = current->next;
+	return (data);
+}
