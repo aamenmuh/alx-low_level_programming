e0e0bd07affd853adba9c0be0ded9bd469aa0d21
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-08-02 00:57:37 +0200

    Create 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - frees list
+ * @head: head
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	current = head;
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+	*head = NULL
+}
