bdef249acca83898ab6dd72f0a598373d5138c6b
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-08-01 23:53:00 +0200

    Create 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * listint_len - returns length of list
+ * @h: head
+ *
+ * Return: length of list
+ */
+size_t listint_len(const listint_t *h)
+{
+	int counter;
+
+	counter = 0;
+	while (h != NULL)
+	{
+		coutner++;
+		h = h->next;
+	}
+	return (counter);
+}
