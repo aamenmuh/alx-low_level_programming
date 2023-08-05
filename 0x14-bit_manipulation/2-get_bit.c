47f5a1ce6647a6f9e4b522e4ea3b2879eb5b38ec
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-08-05 02:58:17 +0200

    Create 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * get_bit - gets bit at index
+ * @n: number
+ * @index: position to be found
+ *
+ * Return: returns bit at index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int size, i;
+	int currentBit;
+
+	size = sizeof(unsigned long int) * 8;
+	if (index > size - 1)
+		return (-1);
+	currentBit = (n >> index) & (1ul);
+	return (currentBit);
+}
