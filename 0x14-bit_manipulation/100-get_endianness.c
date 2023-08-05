bafadb6dca0f743db58484a42e88ae7cf7206d15
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-08-05 04:21:07 +0200

    Create 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * get_endianness - checks endiannes by checking how 1 is stored
+ *
+ * Return: 1 if little, 0 if big
+ */
+int get_endianness(void)
+{
+	unsigned long int n;
+
+	n = 1;
+	return (*((char *)(&n)))
+}
