2b957ef41c70d2ffe463066b3c114acb13610627
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-08-05 03:25:29 +0200

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,7 +2,7 @@
 /**
  * flip_bits - gets numbetr of bits required to flip
  * @n: first num
- * @mm: second num
+ * @m: second num
  *
  * Return: returns number of bits
  */
@@ -12,11 +12,10 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int counter;
 
 	counter = 0;
-	value = 0;
 	value = n ^ m;
 	while (value != 0)
 	{
-		if ((value & 1ul) == 1 )
+		if ((value & 1ul) == 1)
 			counter++;
 		value >>= 1;
 	}
