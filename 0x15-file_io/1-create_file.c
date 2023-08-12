a89b5ba3c3a3cf9da3665202780bc647fdb3b0db
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-08-12 03:38:54 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 /**
  * create_file - creates a files with given content
  * @filename: name of file
@@ -17,19 +19,19 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	openStatus = fopen(filename, O_WRONLY | O_CREATE | O_TRUNC, S_IRUSR | S_IWUSR);
+	openStatus = open(filename, O_WRONLY | O_CREATE | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (openStatus == -1)
 		return (-1);
 	if (text_content == NULL)
 	{
-		fclose(openStatus);
+		close(openStatus);
 		return (1);
 	}
 	length = strlen(text_content);
 	writeBytes = fwrite(openStatus, text_content, length);
 	if (writeBytes != length)
 	{
-		fclose(openStatus);
+		close(openStatus);
 		return (-1);
 	}
 	return (1);
