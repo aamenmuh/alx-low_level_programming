cab01da6eeb6ef9e759a60b7842873c596198903
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-08-12 03:13:13 +0300

    Create 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * read_textfile - reads data from a text file
+ * @filename: name of file
+ * @letters: number of characters to read and print
+ *
+ * Return: returns letters read on success, 0 on failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *file;
+	char *allocation;
+	size_t readBytes;
+
+	if (filename == NULL)
+		return (0);
+	*file = fopen(filename, "r");
+	if (file == NULL)
+		return (0);
+	allocation = malloc(letters + 1);
+	if (allocation == NULL)
+	{
+		fclose(file);
+		return (0);
+	}
+	readBytes = fread(allocation, 1, letters, file);
+	if (readBytes == 0)
+	{
+		free(allocation);
+		fclose(file);
+		return (0);
+	}
+	allocation[readBytes] = '\0';
+
+	if (fwrite(allocation, 1, readBytes, stdout) != readBytes)
+	{
+		free(allocation);
+		fclose(file);
+		return (0);
+	}
+	free(allocation);
+	fclose(file);
+	return (readBytes);
+}
