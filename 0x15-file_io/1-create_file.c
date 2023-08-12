831aca0ac851242015234f2808ca23b2763a3a65
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-08-12 03:39:39 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	openStatus = open(filename, O_WRONLY | O_CREATE | O_TRUNC, S_IRUSR | S_IWUSR);
+	openStatus = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (openStatus == -1)
 		return (-1);
 	if (text_content == NULL)
