f7d3f2d8452ead846cd409b5af146634f86330ff
Aamen Muharram <94831316+aamenmuh@users.noreply.github.com>
2023-08-12 03:33:02 +0300

    Create 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string>
+/**
+ * create_file - creates a files with given content
+ * @filename: name of file
+ * @text_content: content to be written to file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int openStatus;
+	ssize_t length;
+	ssize_t writeBytes;
+
+	if (filename == NULL)
+		return (-1);
+	openStatus = fopen(filename, O_WRONLY | O_CREATE | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (openStatus == -1)
+		return (-1);
+	if (text_content == NULL)
+	{
+		close(openStatus);
+		return (1);
+	}
+	length = strlen(text_content);
+	writeBytes = write(openStatus, text_content, length);
+	if (writeBytes != length)
+	{
+		close(openStatus);
+		return (-1);
+	}
+	return (1);
+}
